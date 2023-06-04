4644310743f7a52f75bcd7fa1b23e3c6d503a18b
Farzine <alfezafarzine@gmail.com>
2023-06-04 17:38:26 +0600

    initializing digit string

--- src/main/java/com/example/calc/Calc.java
+++ src/main/java/com/example/calc/Calc.java
@@ -4,7 +4,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Calc extends Application {
-    
+
+    private static final String[][] template ={ // initializing digit string
+            {"7","8","9","/"},
+            {"4","5","6","*"},
+            {"1","2","3","-"},
+            {"0","clr","=","+"}
+    };
     public static void main(String[] args) {
 
     }
