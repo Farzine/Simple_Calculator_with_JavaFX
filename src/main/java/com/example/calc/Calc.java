949e9d3ab24267b072abab0a3ea5a493bb5c343b
Farzine <alfezafarzine@gmail.com>
2023-06-04 17:32:34 +0600

    Initial commit

---
+++ src/main/java/com/example/calc/Calc.java
@@ -0,0 +1,16 @@
+package com.example.calc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Calc extends Application {
+    
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
